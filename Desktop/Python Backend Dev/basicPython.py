2ac74ac0eb59e61cadba1a97c06291a6ca2e53ad
Chetan Singh <chetan95497@gmail.com>
2022-06-18 15:39:39 +0530

    Tried some basic code

---
+++ Desktop/Python Backend Dev/basicPython.py
@@ -0,0 +1,9 @@
+# print("Hello world \nWelcome")
+# print("I am Chetan and my name is", 100)
+
+name = "Chetan"
+age = 18
+
+print("Hello my name is " +name+ " I am ", age)
+# You cannot concatenate an int and a string
+
