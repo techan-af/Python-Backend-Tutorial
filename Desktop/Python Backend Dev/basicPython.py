9cd549f086500a9c2579ec899788d4426d4a2341
Chetan Singh <chetan95497@gmail.com>
2022-06-19 08:48:57 +0530

    Did some basic string operations and committed

--- Desktop/Python Backend Dev/basicPython.py
+++ Desktop/Python Backend Dev/basicPython.py
@@ -1,9 +1,37 @@
+from math import *
+
+
 # print("Hello world \nWelcome")
 # print("I am Chetan and my name is", 100)
 
-name = "Chetan"
-age = 18
+# name = "Chetan"
+# age = 18
 
-print("Hello my name is " +name+ " I am ", age)
+# print("Hello my name is " +name+ " I am ", age)
 # You cannot concatenate an int and a string
+# You can use backslash to escape the string
 
+
+# print(name[0])
+# print(name.upper())
+# print(name.isupper())
+# print(len(name))
+# print(name.index("h"))
+# print(name.replace("C", "T"))
+
+# number = 77
+# number2 = str(number)
+# print(number +22)
+# print("Number is " +number2)
+# print(abs(-5))
+# Gives the absolute value of the number
+# print(max(4, 2)) # similar for min
+# print(round(3.2)) # there is math.floow() and math.ceil() as well
+# print(bin(212)) # prints binary code for the given number
+# print(sqrt(100))
+
+my_list = [
+    [1, 2, 3],
+    [4, 5, 6]
+] 
+print(my_list)
